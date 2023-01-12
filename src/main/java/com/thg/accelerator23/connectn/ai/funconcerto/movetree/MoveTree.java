72b8385742e3392314e80e8d860526152f1c4993
Bethan Cropper <Bethan.Cropper03@thehutgroup.com>
2023-01-12 11:24:45 +0000

    added all initial fuctionality and testing to MoveTree class

--- src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/MoveTree.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/funconcerto/movetree/MoveTree.java
@@ -7,33 +7,37 @@ import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.GameConfig;
 import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
 
+import static java.util.Collections.min;
+
 public class MoveTree {
     private final Counter counter;
-    private final int depth;
     private final Board state;
     private int mostRecentPosition;
     private List<MoveTree> moveList;
 
     public MoveTree() {
         this.counter = Counter.O;
-        this.depth = 1;
         GameConfig config = new GameConfig(10, 8, 4);
         this.state = new Board(config);
+        this.moveList = new ArrayList<>();
     }
 
-    public MoveTree(Counter counter, int depth, Board state, int position){
+    public MoveTree(Counter counter, Board state, int position){
         this.counter = counter;
-        this.depth = depth;
         this.state = state;
         this.mostRecentPosition = position;
+        this.moveList = new ArrayList<>();
     }
 
+    public MoveTree(MoveTree another) {
+        this.counter = another.getCounter();
+        this.state = getState();
+        this.mostRecentPosition = another.getPosition();
+        this.moveList = another.getChildren();
+    }
 
     public Counter getCounter(){
         return this.counter;
-    }
-    public int getDepth(){
-        return this.depth;
     }
 
     public Board getState(){
@@ -46,19 +50,74 @@ public class MoveTree {
 
     public void addNode(int position) throws InvalidMoveException {
         Counter newCounter = counter.getOther();
-        int newDepth = depth + 1;
         Board newState = new Board(state, position, newCounter);
-        MoveTree newNode = new MoveTree(counter.getOther(), depth + 1, newState, position);
+        MoveTree newNode = new MoveTree(counter.getOther(), newState, position);
+        moveList.add(newNode);
     }
     public void removeNode(List<Integer> moveListToGetToNode){
-        System.out.println("sjhviw");
+        removeNodeRecursion(this, moveListToGetToNode);
+    }
+
+    private void removeNodeRecursion(MoveTree tree, List<Integer> moveListToGetToNode){
+        if (moveListToGetToNode.size() == 1){
+            List<MoveTree> listFromWhichToDeleteMove = tree.getChildren();
+            for (int i = 0; i < listFromWhichToDeleteMove.size(); i++) {
+                if (moveListToGetToNode.get(0) == listFromWhichToDeleteMove.get(i).getPosition()){
+                    listFromWhichToDeleteMove.remove(i);
+                    break;
+                }
+            }
+        } else {
+            int nextNodeIndex = moveListToGetToNode.get(0);
+            moveListToGetToNode.remove(0);
+            removeNodeRecursion(tree.getChildAtPosition(nextNodeIndex), moveListToGetToNode);
+        }
     }
     public List<MoveTree> getLeaves(){
+        MoveTree treeCopy = new MoveTree(this);
         List<MoveTree> leafList = new ArrayList<>();
+        if (treeCopy.getChildren().size() == 0) {
+            leafList.add(this);
+        }
+
+        while (treeCopy.getChildren().size() != 0) {
+            leafList.add(treeCopy.popLeftLeaf());
+        }
+
         return leafList;
     }
     public List<MoveTree> getChildren(){
         return moveList;
     }
 
+    private MoveTree popLeftLeafNode(MoveTree tree,MoveTree previousTree, List<Integer> traversePath) {
+        if (tree.getChildren().isEmpty()) {
+            MoveTree nodeCopy = new MoveTree(tree);
+            previousTree.removeNode(traversePath);
+            return nodeCopy;
+        } else {
+            ArrayList<Integer> childPositions = new ArrayList<>();
+            for (MoveTree child : tree.getChildren()) {
+                childPositions.add(child.getPosition());
+            }
+            int leftPosition = min(childPositions);
+            List<Integer> path = new ArrayList<Integer>();
+            path.add(leftPosition);
+            return popLeftLeafNode(tree.getChildAtPosition(leftPosition),tree, path);
+        }
+    }
+
+    public MoveTree popLeftLeaf(){
+        return popLeftLeafNode(this, this, new ArrayList<>());
+    }
+
+    public MoveTree getChildAtPosition(int position) throws IndexOutOfBoundsException {
+        List<MoveTree> children = this.getChildren();
+        for( MoveTree child : children){
+            if (child.getPosition() == position){
+                return child;
+            }
+        }
+        throw new IndexOutOfBoundsException();
+    }
 }
